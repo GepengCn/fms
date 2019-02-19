20cec6ba2581e22bd1b24365bd7a7ec1fcfdc2dd
GepengCn <214764500@qq.com>
2019-02-19 14:00:01 +0800

    更新文档;引入zip工具

--- src/main/java/com/itonglian/fms/dbstep/TestDBSTEP.java
+++ src/main/java/com/itonglian/fms/dbstep/TestDBSTEP.java
@@ -6,7 +6,7 @@ public class TestDBSTEP {
 
 
     public static void main(String[] args) throws IOException {
-        File file = new File("/Users/gepeng/Downloads/encrypt.doc");
+        File file = new File("/Users/gepeng/Downloads/ftpLocation/1`FE#ENC#0DE5B47DB2136CD6BC519231BA1A54E8");
 
         FileInputStream fis = new FileInputStream(file);
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -20,7 +20,7 @@ public class TestDBSTEP {
         MsgObj.MsgFileBody(b);
         b = MsgObj.ToDocument(MsgObj.MsgFileBody());
         MsgObj.MsgFileBody(b);
-        if(MsgObj.MsgFileSave("/Users/gepeng/Downloads/decrypt.doc")){
+        if(MsgObj.MsgFileSave("/Users/gepeng/Downloads/decrypt.docx")){
             System.out.println("success");
         }
     }
--- src/main/java/com/itonglian/fms/service/common/impl/AttPieceTask.java
+++ src/main/java/com/itonglian/fms/service/common/impl/AttPieceTask.java
@@ -20,5 +20,4 @@ public class AttPieceTask extends PieceTask{
         log.info("AttPieceTask over");
     }
 
-
 }
