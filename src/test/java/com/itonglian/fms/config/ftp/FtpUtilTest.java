ca9b145f16ef00c4d9f6dc3d448f067a1af2c89d
GepengCn <214764500@qq.com>
2019-03-14 17:48:03 +0800

    测试用例

--- src/test/java/com/itonglian/fms/config/ftp/FtpUtilTest.java
+++ src/test/java/com/itonglian/fms/config/ftp/FtpUtilTest.java
@@ -7,6 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
+import java.nio.charset.Charset;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.SortedMap;
 import java.util.UUID;
 
 import static org.junit.Assert.*;
@@ -25,8 +29,14 @@ public class FtpUtilTest {
 
     @Test
     public void download() {
-        String ftpPath = "/104e1557-9b4d-4c2f-bb3b-d2a9d36d6b65/069b0cad-a29f-44df-873e-63acb3ec4b3d/";
+        /*String ftpPath = "/104e1557-9b4d-4c2f-bb3b-d2a9d36d6b65/069b0cad-a29f-44df-873e-63acb3ec4b3d/";
         String destPath = "/Users/gepeng/Downloads/template";
-        ftpUtil.download(ftpPath,destPath);
+        ftpUtil.download(ftpPath,destPath);*/
+        SortedMap<String,Charset> stringCharsetSortedMap =  Charset.availableCharsets();
+        Collection<Charset> charsets = stringCharsetSortedMap.values();
+        Iterator<Charset> iterator = charsets.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next().name());
+        }
     }
 }
